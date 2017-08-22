cc32ab9467b071069aa14281c440ffc317fdb55d
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-08-22 10:48:49 +0700

    create new Resource Controllers

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Studtent;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Studtent  $studtent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Studtent $studtent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Studtent  $studtent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Studtent $studtent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Studtent  $studtent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Studtent $studtent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Studtent  $studtent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Studtent $studtent)
+    {
+        //
+    }
+}
